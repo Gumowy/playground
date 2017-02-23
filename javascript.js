b85c7cd19b9b6e2280b0465a5b4823b7eb778c37
Gumowy <Q@MacBook-Air.local>
2017-02-23 22:51:33 +0100

    m

--- javascript.js
+++ javascript.js
@@ -23,25 +23,6 @@ $("ul#hamburger_menu").hide();
 });
 
 
-
-
-
-$(document).ready(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-
 $(document).ready(function(){
 
     $("li.hamburger_list").click(function(){
